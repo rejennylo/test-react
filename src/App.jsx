1837b6e04205eeb5dc2e54375314402f956925a3
rejennylo <rejennylo@gmail.com>
2023-11-23 15:13:02 +0800

    /test-react/

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <img src="test-react/assets/images/Img-activity03.jpg" alt="" />
+      <img src="/test-react/assets/images/Img-activity03.jpg" alt="" />
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
